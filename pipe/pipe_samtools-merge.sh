15066525d58ebfcdc91d31282cb5ea227187c7b0
kimzz14 <kimzz1004@gmail.com>
2024-12-16 23:34:42 +0900

    bug

--- pipe/pipe_samtools-merge.sh
+++ pipe/pipe_samtools-merge.sh
@@ -2,7 +2,7 @@
 readID=$1
 threadN=$2
 
-readDir=/archive/kimzz14/SRA_RAW/NAAS/Triticum_aestivum/Keumkang/Iso-Seq/data
+readDir=/archive/kimzz14/SRA_RAW/NAAS/Triticum_aestivum/Keumkang/IsoSeq/data
 ############################################################################################
 
 samtools merge \
